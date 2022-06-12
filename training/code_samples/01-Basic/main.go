0d4be1ab53291b69f7d57324772e98a7ba920e20
FETHI ATAC <fethi.ata@gmail.com>
2022-06-12 20:09:49 +0300

    Go_Step1

--- training/code_samples/01-Basic/main.go
+++ training/code_samples/01-Basic/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var x int
+
+type customType int
+
+var y customType
+
 func main() {
 	fmt.Println("hello golang")
+	z := "first string variable"
+	x = 12345
+	y = 98765
+	fmt.Printf("%T %v \n", x, x)
+	fmt.Printf("%T %v \n", y, y)
+	fmt.Printf("%T %v \n", z, z)
+	foo()
+
+}
+func foo() {
+	fmt.Println("foo func calling")
 }
